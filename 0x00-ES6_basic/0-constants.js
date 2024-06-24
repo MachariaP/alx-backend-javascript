45e7d1e5fb3adb13efddc10f419af82599e94fc9
MachariaP <walburphinehas78@gmail.com>
2024-06-24 23:27:14 +0300

    Modify
    function taskFirst to instantiate variables using const
    function taskNext to instantiate variables using let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
-export const taskFirst = () => {
-    const tasks = 'I prefer const when I can.';
-    return tasks;
-};
-
-export function getLast() {
-    return 'is okay';
-}
-
-export const taskNext = () => {
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-
+  
     return combination;
-};
+  }
