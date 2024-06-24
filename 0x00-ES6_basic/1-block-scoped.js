e43e0bd19b5620b448c2f6c0b3d36f8e6a61f223
MachariaP <walburphinehas78@gmail.com>
2024-06-24 23:57:55 +0300

    Given what you’ve read about var and hoisting, modify the variables inside the function taskBlock so that the variables aren’t overwritten inside the conditional block.

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueorfalse) {
+    let task =false;
+    let task2 = true;
+
+    if (trueorfalse) {
+        let task = true;
+        let task2 = false;
+    }
+
+    return [task, task2];
+}
+
