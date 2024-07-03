2fa109a671ed91155ae050614224a71d89978bb6
MachariaP <walburphinehas78@gmail.com>
2024-07-03 03:37:33 +0300

    Fix: To handle validation of set and startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,8 @@
 function cleanSet(set, startString) {
-  // Validation: Check if 'set' is a Set
   if (!(set instanceof Set)) {
     throw new TypeError('The first argument must be a Set');
   }
 
-  // Validation: Check if 'startString' is a string
   if (typeof startString !== 'string') {
     throw new TypeError('The second argument must be a string');
   }
