784001df2d9f65805034ddb091f9bc250e82aa76
MachariaP <walburphinehas78@gmail.com>
2024-07-03 03:02:17 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  return [...set]
+    .filter(item => item.startsWith(startString))
+    .map(item => item.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
