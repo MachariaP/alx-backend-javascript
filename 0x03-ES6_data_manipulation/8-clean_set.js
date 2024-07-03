31c0db34a43357fc8ac0fea8475970e2f60a7f06
MachariaP <walburphinehas78@gmail.com>
2024-07-03 08:50:56 +0300

    Fix: msg - Execution not successful

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,15 @@
 function cleanSet(set, startString) {
+  // Check if the first argument is a Set
   if (!(set instanceof Set)) {
     throw new TypeError('The first argument must be a Set');
   }
 
-  if (typeof startString !== 'string') {
-    throw new TypeError('The second argument must be a string');
+  // Return an empty string if startString is not provided or not a string
+  if (typeof startString !== 'string' || !startString) {
+    return '';
   }
 
-  if (!startString) return '';
-
+  // Process the set
   return [...set]
     .filter((item) => typeof item === 'string' && item.startsWith(startString))
     .map((item) => item.slice(startString.length))
