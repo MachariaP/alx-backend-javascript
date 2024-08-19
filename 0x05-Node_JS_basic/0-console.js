e7c8db455311bb1eb8707212345ea3c7f996138c
MachariaP <walburphinehas78@gmail.com>
2024-08-19 11:56:39 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,11 @@
-// 0-console.js
+/**
+ * Function that displays a message in the console
+ * @param {string} message - The message to display
+ * @returns {void}
+ */
 function displayMessage(message) {
     console.log(message);
 }
 
+// Export the function
 module.exports = displayMessage;
